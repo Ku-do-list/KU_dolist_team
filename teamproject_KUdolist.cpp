8710662911014ef3d28ed5fa3273caea0844f239
ChanYong Park <cksdyd0654@gmail.com>
2022-10-24 00:29:36 +0900

    commit

--- teamproject_KUdolist.cpp
+++ teamproject_KUdolist.cpp
@@ -21,6 +21,7 @@ using namespace std;
 	writeFileList.close();
 }
 
+
 void create_Categoryfile(vector <string> outCategory)
 {
 	ofstream writeFileCategory;
@@ -41,18 +42,30 @@ int main(void)
 {
 	char c; // 입력할 때 사용
 	bool iter; // 반복여부 확인
-	iter = true; // 반복 없음이 디폴트
+	iter = false; // 반복 없음이 디폴트
 	string name; // 일정 이름
 	string category; // 카테고리 이름
-	string dateNtime; // 반복없는 날짜 및 종시간
+	string dateNtimeFirst; // 반복없는 날짜 및 시작시간
+	string dateNtimeLast; // 반복없는 날짜 및 종료시간
 	string dateNtimeIter; // 반복 있는 일정 추가 시 날짜 및 시간
-	
+	string Iter;
+	string Done;
+	int num = 0;
 	vector<string> outname; // 출력할 때의 배열
 	vector<string> outcategory; // 출력할 때의 배열
-	vector<string> outdateNtime; // 출력할 때의 배열
+	vector<string> outdateNtimeFirst; // 출력할 때의 배열
+	vector<string> outdateNtimeLast; // 출력할 때의 배열
 	vector<string> outdateNtimeIter; // 출력할 때의 배열
-
+	vector<string> outIter;// 출력할 때의 배열
+	vector<string> outDone;// 출력할 때의 배열
 	vector<int>outindex;
+	
+	vector<vector<string>> all(100); //전체 배열
+
+	//vector<string> allin;
+	//all.push_back(allin);
+
+	int ai=0; // 전체배열 index
 	
 	int index; // 찾는 값의 인덱스
 	string key; //찾는 내용
@@ -83,16 +96,35 @@ int main(void)
 		writeFileList.put(c);
 	}
 	writeFileList.put('/');
-	
+
 	cout << "카테고리이름 " << endl;
-	while (cin.get(c) && c != '\n' ) // 카테고리 파일에 입력 / 엔터입력하면 종료
+	while (cin.get(c) && c != '\n') // 카테고리 파일에 입력 / 엔터입력하면 종료
 	{
 		writeFileCategory.put(c);
+		writeFileList.put(c);
 	}
+	writeFileList.put('/');
 	writeFileCategory.put('\n');
-
 	
-	if (!iter) // 반복 있으면
+	cout << "반복여부 " << endl;
+	while (cin.get(c) && c != '\n') // 일정 파일에 입력 / 엔터입력하면 종료 / 반복기능 입력
+	{
+		writeFileList.put(c);
+		if (c != '4')
+		{
+			iter = true;
+		}
+	}
+	writeFileList.put('/');
+
+	cout << "일정완료여부 " << endl;
+	while (cin.get(c) && c != '\n') // 일정 파일에 입력 / 엔터입력하면 종료 / 일정완료여부 입력
+	{
+		writeFileList.put(c);
+	}
+	writeFileList.put('/');
+	
+	if (iter) // 반복 있으면
 	{
 		cout << "날짜 " << endl;
 		while (cin.get(c) && c != '\n') // 일정 파일에 입력 / 엔터입력하면 종료 / 시작 날짜
@@ -117,8 +149,6 @@ int main(void)
 		}
 		writeFileList.put('\n');
 	}
-
-
 	writeFileList.close(); // 일정 파일 닫기
 	writeFileCategory.close(); // 카테고리 파일 닫기
 	
@@ -140,45 +170,45 @@ int main(void)
 		exit(1);
 	}
 
-	if (!iter) // 반복있음
+	
+
+	if (readFileList.is_open())
 	{
-		if (readFileList.is_open())
+		while (1) // 일정 파일 끝까지 읽었는지 확인
 		{
-
-			while (1) // 일정 파일 끝까지 읽었는지 확인
+			getline(readFileList, name, '/');
+			all[ai].push_back(name);
+			getline(readFileList, category, '/');
+			getline(readFileList, Iter, '/');
+			all[ai].push_back(Iter);
+			if (Iter != "4")
 			{
-				getline(readFileList, name, '/');
-				outname.push_back(name);
+				getline(readFileList, Done, '/');
+				all[ai].push_back(Done);
 				getline(readFileList, dateNtimeIter);
-				outdateNtimeIter.push_back(dateNtimeIter);
-				if (readFileList.eof())
-					break;
-			}
+				all[ai].push_back(dateNtimeIter);
 
-		}
+			}
+			else
+			{
+				getline(readFileList, Done, '/');
+				all[ai].push_back(Done);
+				getline(readFileList, dateNtimeFirst, '&');
+				all[ai].push_back(dateNtimeFirst);
+				getline(readFileList, dateNtimeLast, '\n');
+				all[ai].push_back(dateNtimeLast);
+			}
+			ai++;
+			if (readFileList.eof())
+			{
+				break;
+			}
+		}	
 	}
 	
-	else //반복없음
-	{
-		if (readFileList.is_open())
-		{
-
-			while (1) // 일정 파일 끝까지 읽었는지 확인
-			{
-				getline(readFileList, name, '/');
-				outname.push_back(name);
-				getline(readFileList, dateNtime); // 데이터 파일에서 /로 파싱할 때에 시작 날짜 시간이란 마감 날짜 시간의 인덱스가 다를 수 있음 그래서 &로 설정
-				outdateNtime.push_back(dateNtime);
-				if (readFileList.eof())
-					break;
-			}
-
-		}
-	}
 
 	if (readFileCategory.is_open())
 	{
-		
 		while (1)
 		{
 			getline(readFileCategory, category);
@@ -187,59 +217,12 @@ int main(void)
 				break;
 		}
 	}
-
-	/* 
-	for (int i = 0; i < outname.size(); i++) // 전체 목록을 다 출력하려고 하면 날짜가 마지막 저장된 날짜가 두 번 반복되는 현상이 있음.. 인덱스로 찾으면 문제 없음
-	{
-		cout << outname[i] << endl;
-	}
-	cout << endl;
-	for (int i = 0; i < outdateNtimeIter.size(); i++)
-	{
-		cout << outdateNtimeIter[i] << endl;
-	}
-	cout << endl;
+	
 	for (int i = 0; i < outcategory.size(); i++)
 	{
-		cout << outcategory[i] << endl;;
+		cout << outcategory[i] << endl;
 	}
-	*/
-
-	cout << "찾는 이름?" << endl;
-	cin >> key;
-
-	for (int j = 0; j < outname.size(); j++)
-	{
-		if (outname[j] == key)
-		{
-			outindex.push_back(j);
-		}
-	}
-
-	for (int j = 0; j < outindex.size(); j++)
-	{
-		index = outindex[j];
-		cout << outname[index] << endl;
-	}
-	for (int j = 0; j < outindex.size(); j++)
-	{
-		index = outindex[j];
-		cout << outcategory[index] << endl;
-	}
-	for (int j = 0; j < outindex.size(); j++)
-	{
-		index = outindex[j];
-		cout << outdateNtime[index] << endl;
-	}
-	//outname.erase(outname.begin() + 1);
-	/*
-	if (replace)
-	{
-		cout << "수정할게용" << endl;
-		create_Listfile(outname, outdateNtime);
-		create_Categoryfile(outcategory);
-	}
-	*/
+	
 	readFileList.close(); // 일정 파일 닫기
 	readFileCategory.close(); // 카테고리 파일 닫기
 	
